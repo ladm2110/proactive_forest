68977bded5fee8ae5a82c0dc0674ea961a0469cc
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2017-10-28 20:24:44 -0400

    In entropy calculation, we have to remove all ceros first, auxiliar list and remover added.

--- proactive_forest/metrics.py
+++ proactive_forest/metrics.py
@@ -23,5 +23,13 @@ def entropy(x):
     if len(x) == 0:
         return 0.0
     counts = np.bincount(x)
+
+    aux = list()
+    for c in counts.tolist():
+        if c != 0:
+            aux.append(c)
+    counts = np.array(aux)
+
     p = counts / float(len(x))
+    p.dumps()
     return -np.sum(p * np.log(p))
