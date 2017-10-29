89db5456b1d261793e036d8f8cb6f31d670a86f7
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2017-10-28 20:28:22 -0400

    Some modifications to the tree growing algorithm, it can work with categorical values, added some new parameters.

--- proactive_forest/tree_builder.py
+++ proactive_forest/tree_builder.py
@@ -1,15 +1,15 @@
 import numpy as np
 import scipy.stats
-import logging
 from random import choices
 import pandas as pd
 
-from proactive_forest.metrics import list_to_discrete_dist
-from proactive_forest.tree import BinaryDecisionTree, BinaryDecisionTreeSplit
+from proactive_forest.tree import BinaryDecisionTree, BinaryDecisionTreeSplit, DecisionTree, DecisionLeaf, DecisionFork, \
+    DecisionForkCategorical
 
 from multiprocessing import Pool
 
-from proactive_forest.utils import split_dataset, compute_split_info, compute_split_gain, SplitCriterion
+from proactive_forest.utils import compute_split_info, compute_split_gain, SplitCriterion, split, \
+    split_categorical
 
 
 class TreeSplit(BinaryDecisionTreeSplit):
@@ -18,51 +18,23 @@ class TreeSplit(BinaryDecisionTreeSplit):
         self.gain = gain
 
 
-class TreeBuilder:
-    """
-    TreeBuilder implements an algorithm (currently, without pruning phase)
-    for building decision trees.
-    """
-
-    logger = logging.getLogger("TreeBuilder")
-    debug = False
-
+class Builder:
     def __init__(self,
                  max_depth=10,
+                 min_samples_split=2,
                  min_samples_leaf=5,
                  max_n_splits=None,
                  criterion='gini',
                  feature_selection='all',
                  feature_prob=None,
+                 min_gain_split=0.05,
+                 categorical=[],
                  n_jobs=1):
-        """
-        Initializes proactive_forest new tree builder and validates the parameters.
-
-        Args:
-            problem (string):
-                Can be either 'classification' or 'regression'
-
-            max_depth (int): default is 10
-                A stopping criteria, the maximum depth of the tree
-
-            min_samples_per_leaf (int): default is 5
-                A stopping criteria, stop building the subtree if
-                the number of samples in it is less than `min_samples_per_leaf`
-
-            max_n_splits (int): default is 1
-                Number of splitting values to consider when choosing the best split
-
-            criterion (string):
-                A criterion used for estimating quality of a split.
-                It can be either 'gini' or 'entropy'.
-
-            n_jobs (int): the size of process pool to use when building the tree.
-                When None, use the number of cores in the system
-        """
-
         self.max_depth = max_depth
         self.max_n_splits = max_n_splits
+        self.min_samples_split = min_samples_split
         self.min_samples_leaf = min_samples_leaf
+        self.min_gain_split = min_gain_split
 
         assert feature_selection in ['all', 'rand', 'prob']
         self.feature_selection = feature_selection
@@ -71,79 +43,75 @@ class TreeBuilder:
         assert criterion in [SplitCriterion.GINI, SplitCriterion.ENTROPY]
         self.split_criterion = SplitCriterion.resolve_split_criterion(criterion)
 
+        self.categorical = categorical
+
         self.n_jobs = n_jobs
         self.pool = None
 
     def build_tree(self, X, y):
-        """
-        Args:
-            X: object-features matrix
-            y: target vector
-
-        Returns:
-            A `BinaryDecisionTree` fitted to the dataset.
-
-            The actual structure of the tree depends both on dataset and the parameters
-            passed to the `TreeBuilder` constructor.
-
-        """
         n_samples, n_features = X.shape
-        tree = BinaryDecisionTree(n_features=n_features)
+        tree = DecisionTree(n_features=n_features)
         if self.n_jobs > 1:
             self.pool = Pool(self.n_jobs)
 
-        leaf_to_split = tree.root()
-        self._build_tree_recursive(tree, leaf_to_split, X, y)
-        self._prune_tree(tree, X, y)
-        if TreeBuilder.debug:
-            TreeBuilder.logger.debug(tree)
+        tree.last_node_id = tree.root()
+        self._build_tree_recursive(tree, tree.last_node_id, X, y, depth=1)
+        # self._prune_tree(tree, X, y)
         self.pool = None
         return tree
 
-    def _build_tree_recursive(self, tree, cur_node, X, y):
-
+    def _build_tree_recursive(self, tree, cur_node, X, y, depth):
         n_samples, n_features = X.shape
-
-        # Evaluates if there is no instance
-        if n_samples == 0:
-            return
-
         leaf_reached = False
 
+        # Evaluates if all instances belong to the same class
+        if self._all_same_class(y):
+            leaf_reached = True
+
         # Evaluates the min_samples_leaf stopping criteria
-        if n_samples <= self.min_samples_leaf:
+        # it should be min_samples_split
+        if n_samples < self.min_samples_split:
             leaf_reached = True
 
         # Evaluates the depth stopping criteria
-        depth = tree.depth(cur_node)
         if self.max_depth is not None and depth >= self.max_depth:
             leaf_reached = True
 
         best_split = None
         if not leaf_reached:
-            if TreeBuilder.debug:
-                TreeBuilder.logger.debug('Split at {}, n = {}'.format(cur_node, n_samples))
-
             best_split = self._find_best_split(X, y)
-            if best_split is None:
+            if best_split is None or best_split.gain < self.min_gain_split:
                 leaf_reached = True
 
-        tree._leaf_n_samples[cur_node] = len(y)
         if leaf_reached:
-            tree._leaf_values[cur_node] = scipy.stats.mode(y).mode[0]
-        else:
-            tree.split_node(cur_node, best_split)
+            result = scipy.stats.mode(y)
+            probability = result.count[0]/len(y)
+            tree.nodes.append(DecisionLeaf(result=result.mode[0], prob=probability, n_samples=len(y), depth=depth))
 
-            left_child = tree.left_child(cur_node)
-            right_child = tree.right_child(cur_node)
-            X_left, X_right, y_left, y_right = split_dataset(
-                    X, y, best_split.feature_id, best_split.value)
-            self._build_tree_recursive(tree, left_child, X_left, y_left)
-            self._build_tree_recursive(tree, right_child, X_right, y_right)
+        else:
+            if best_split.feature_id in self.categorical:
+                tree.nodes.append(DecisionForkCategorical(n_samples=len(y), depth=depth,
+                                                          feature_id=best_split.feature_id, gain=best_split.gain))
+                for key, X_new, y_new in split_categorical(X, y, best_split.feature_id):
+                    tree.last_node_id += 1
+                    node_to_split = tree.last_node_id
+                    tree.nodes[cur_node].add(key, self._build_tree_recursive(tree, node_to_split, X_new, y_new, depth=depth + 1))
+            else:
+                tree.nodes.append(DecisionFork(n_samples=len(y), depth=depth, feature_id=best_split.feature_id,
+                                               value=best_split.value, gain=best_split.gain))
+                for X_new, y_new in split(X, y, best_split.feature_id, best_split.value):
+                    tree.last_node_id += 1
+                    node_to_split = tree.last_node_id
+                    tree.nodes[cur_node].add(self._build_tree_recursive(tree, node_to_split, X_new, y_new, depth=depth+1))
+
+        return cur_node
 
     def _prune_tree(self, tree, X, y):
         # TODO: add tree pruning
         pass
+
+    def _all_same_class(self, y):
+        return scipy.stats.mode(y).count[0] == len(y)
 
     def _compute_split_values(self, X, y, feature_id):
         x = X[:, feature_id]
@@ -165,8 +133,11 @@ class TreeBuilder:
         features = self._select_features(self.feature_selection, n_features)
 
         for feature_id in features:
-            for split_value in self._compute_split_values(X, y, feature_id):
-                args.append([self.split_criterion, X, y, feature_id, split_value])
+            if feature_id in self.categorical:
+                args.append([self.split_criterion, X, y, feature_id, None])
+            else:
+                for split_value in self._compute_split_values(X, y, feature_id):
+                    args.append([self.split_criterion, X, y, feature_id, split_value])
         if self.pool is not None:
             split_infos = self.pool.map(compute_split_info, args)
         else:
@@ -174,13 +145,16 @@ class TreeBuilder:
 
         splits = []
         for arg, split_info in zip(args, split_infos):
-            _, _, _, feature_id, split_value = arg
-            gain, n_left, n_right = split_info
-            if n_left < self.min_samples_leaf or n_right < self.min_samples_leaf:
+            if split_info is not None:
+                _, _, _, feature_id, split_value = arg
+                gain, n_min = split_info
+                if n_min < self.min_samples_leaf:
+                    continue
+                if gain is not None and gain > 0:
+                    split = TreeSplit(feature_id, value=split_value, gain=gain)
+                    splits.append(split)
+            else:
                 continue
-            if gain is not None and gain > 0:
-                split = TreeSplit(feature_id, value=split_value, gain=gain)
-                splits.append(split)
 
         # Find best split
         best_split = None
@@ -202,4 +176,5 @@ class TreeBuilder:
             weights = self.feature_prob
             selected = choices(population, weights, k=n_features)
             return pd.unique(selected)
+
 
