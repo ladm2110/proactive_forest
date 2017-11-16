a7449642fe5ef1a70da14f7a9e4dfd0596b26318
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2017-11-16 08:50:18 -0500

    Remove list_to_discrete method. Unused.

--- proactive_forest/metrics.py
+++ proactive_forest/metrics.py
@@ -1,16 +1,6 @@
 import numpy as np
 
 
-def list_to_discrete_dist(x):
-    """
-    Takes a list of positive ints and turns it into a discrete distribution
-    E.g.: list_to_discrete_dist([0,0,1,1,2]) => ([0, 1, 2], [0.4, 0.4, 0.2])
-    """
-    counts = np.bincount(x)
-    unique_indices = np.nonzero(counts)[0]
-    return unique_indices, counts[unique_indices] / float(x.size)
-
-
 def gini_index(x):
     if len(x) == 0:
         return 0.0
