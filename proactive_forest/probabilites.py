ef1a4919208b646f0592d3e0ef951bd94548e930
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2018-01-09 14:27:49 -0500

    Added new probalities handler module

---
+++ proactive_forest/probabilites.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+
+
+class ProbabilityLedger(ABC):
+    def __init__(self, probabilities, n_features, alpha=5):
+        if probabilities is None:
+
+            if n_features is not None:
+                initial_p = 1 / n_features
+                self.probabilities = [initial_p for _ in range(n_features)]
+            else:
+                raise Exception('Cannot initialize ledger without the number of features.')
+        else:
+
+            if len(probabilities) == n_features:
+                self.probabilities = probabilities
+            else:
+                raise Exception('Number of features must be equal to length of list of probabilities.')
+
+        self.n_features = n_features
+        self.alpha = alpha
+        super().__init__()
+
+    @abstractmethod
+    def update_probabilities(self, features_and_levels):
+        pass
+
+    def _fill_remainder(self):
+        remainder = self._calculate_remainder()
+        split_remainder = remainder / self.n_features
+
+        for p in self.probabilities:
+            p += split_remainder
+
+    def _calculate_remainder(self):
+        remainder = 1
+        for p in self.probabilities:
+            remainder -= p
+
+        return remainder
+
+
+class ModerateLedger(ProbabilityLedger):
+    def __init__(self, probabilities, n_features, alpha=5):
+        super().__init__(probabilities, n_features, alpha)
+
+    def update_probabilities(self, features_and_levels):
+        for feature, level in features_and_levels:
+            old_prob = self.probabilities[feature]
+            score = self.alpha * level
+            new_prob = old_prob * (1 - 1 / score)
+            self.probabilities[feature] = new_prob
+
+        self._fill_remainder()
+
+
+class AggressiveLedger(ProbabilityLedger):
+    def __init__(self, probabilities, n_features, alpha=5):
+        super().__init__(probabilities, n_features, alpha)
+
+    def update_probabilities(self, features_and_levels):
+        for feature, level in features_and_levels:
+            old_prob = self.probabilities[feature]
+            score = self.alpha + level
+            new_prob = old_prob * (1 - 1 / score)
+            self.probabilities[feature] = new_prob
+
+        self._fill_remainder()
