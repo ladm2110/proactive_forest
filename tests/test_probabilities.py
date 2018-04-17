139a5b0e49bd96968154c5f998a1381d586c8486
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2018-04-17 10:34:24 -0400

    Create test_probabilities

---
+++ tests/test_probabilities.py
@@ -0,0 +1,12 @@
+from unittest import TestCase, mock
+
+
+class FIProbabilityLedgerTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_correctly_updating_probabilities(self):
+        pass
