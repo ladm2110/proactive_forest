745bc4def73b70b95c4d4120409b98b4c61d8ee8
Rafael <rafahperez96@gmail.com>
2018-04-17 15:49:26 -0400

    Created FIProbabilityLedger Test

--- tests/test_probabilities.py
+++ tests/test_probabilities.py
@@ -1,12 +1,23 @@
+import numpy as np
 from unittest import TestCase, mock
+from proactive_forest.tree import DecisionTree
+from proactive_forest.probabilites import FIProbabilityLedger
 
 
 class FIProbabilityLedgerTest(TestCase):
     def setUp(self):
-        pass
+        self.tree = mock.MagicMock(spec=DecisionTree)
+        self.tree.feature_importances.return_value = np.array([0.2, 0.2, 0.6])
+        self.ledger = FIProbabilityLedger(probabilities=[0.33, 0.33, 0.33], n_features=3)
 
     def tearDown(self):
         pass
 
     def test_correctly_updating_probabilities(self):
-        pass
+
+        self.ledger.update_probabilities(self.tree, 1/100)
+        result_probabilities = self.ledger.probabilities.tolist()
+        expected = [0.3334, 0.3334, 0.3332]
+
+        for i, j in zip(result_probabilities, expected):
+            self.assertAlmostEqual(i, j, places=4)
