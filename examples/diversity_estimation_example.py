7c7cba988636b3e5dbdcb601e10149986196eecf
Luis Alberto Denis Miranda <ladm2110@gmail.com>
2018-06-06 21:13:53 -0400

    finished some examples of use

--- examples/diversity_estimation_example.py
+++ examples/diversity_estimation_example.py
@@ -1,7 +1,6 @@
 from sklearn.model_selection import train_test_split
 from examples import load_data
 from proactive_forest.estimator import DecisionForestClassifier, ProactiveForestClassifier
-import pandas as pd
 
 
 if __name__ == '__main__':
@@ -10,8 +9,8 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=4)
 
-    pf = ProactiveForestClassifier(alpha=0.1, bootstrap=False)
-    rf = DecisionForestClassifier(split_chooser='best', feature_selection='log', bootstrap=False)
+    pf = ProactiveForestClassifier(alpha=0.1, bootstrap=True)
+    rf = DecisionForestClassifier(split_chooser='best', feature_selection='log', bootstrap=True)
 
     pf.fit(X_train, y_train)
     print('Processed: Proactive Forest')
